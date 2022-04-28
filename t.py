87832c63dbc9a09afc18e4ed69ce688a8f81995c
Nate Balmain <balmainn@sou.edu>
2022-04-28 14:36:25 -0700

    edited makefile for gdb tokens and added regex function names and number breaking

--- t.py
+++ t.py
@@ -1,5 +1,6 @@
 #gdb -q ./simple_program -x t.py 
 from dis import disassemble
+import re
 import gdb
 #from gdb import pretty_printer as pp
 from appJar import gui
@@ -8,14 +9,55 @@ def setbreakpoint():
     break_num = app.getEntry("breakpoint")
     gdb.execute("b {break_num}")
 
+def breakfunc():
+    gdb.execute('b fuction')
+
 def breakmain():
     gdb.execute('b main')
+def getAllFunctions():
+    o = gdb.execute('info functions', to_string = True)
+    s = o.splitlines()
+    print(s)
+    nbs = 'Non-debugging symbols:'
+    ctr = 0
+    getFuncNameRE = ' .*\('
+    getFuncNumberRE = '.*:'
+    for line in s:
+        if line == nbs :
+            #allstring = s[3:ctr]
+            allstring = s[3:ctr-1]
+            break
+        else:
+            ctr=ctr+1    
+    print(allstring)
+    funcNumbers = []
+    funcNames = []
+    for line in allstring:
+        #fnumber = re.search(getFuncNumberRE, line).group()
+        #print(fnumber)
+        #funcNumbers.append(fnumber[:-1])
+        #print(funcNumbers)
+        fname = re.search(getFuncNameRE,line).group()
+        #print (fname)
+        funcNames.append(fname[1:-1])
+        #print(funcNames)
+    #breakAllFunctionsByName(funcNames)
+    breakAllFunctionsByNUmber(funcNumbers)
+    
+def breakAllFunctionsByName(funcNames):
+    for name in funcNames:
+        gdb.execute(f'b {name}')
+
+def breakAllFunctionsByNUmber(funcNumbers):
+    for num in funcNumbers:
+        gdb.execute(f'b {num}')
+
 
 def disassemble_main():
-    o = gdb.execute('disassemble main', to_string=True)
+    #o = gdb.execute('disassemble main', to_string=True)
     #print(len(o))
-    print(type(o))
-    #gdb.execute('disassemble main')
+    #print(type(o))
+    gdb.execute('disassemble main')
 
 def next ():
     o = gdb.execute('n', to_string=True)
@@ -34,7 +76,9 @@ app = gui("gui name here", "400x200")
 app.addButton("next", next)
 app.addButton("run", run)
 app.addButton("break main", breakmain)
+app.addButton("break func", breakfunc)
 app.addButton("disassemble main", disassemble_main)
+app.addButton("get all functions", getAllFunctions)
 app.addButton("quit", quit)
 
 #sets a breakpoint
@@ -65,3 +109,4 @@ HelloCommand()
 #all_objects = muppy.get_objects()
 #summ = summary.summarize(all_objects)
 #summary.print_(summ)
+
